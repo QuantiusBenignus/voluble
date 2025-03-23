b1a412dc563ed6154765a8a63323461a9d730bd0
QuantiusBenignus <120202899+QuantiusBenignus@users.noreply.github.com>
2025-03-23 13:22:14 -0400

    summarize selected text
    Now uses a local LLM to summarize selected text
    Some GUI improvements
    Added Stop Speaking menu item

--- extension.js
+++ extension.js
@@ -23,110 +23,134 @@ const Main = imports.ui.main;
 const MTray = imports.ui.messageTray;
 const PanelMenu = imports.ui.panelMenu;
 const PopupMenu = imports.ui.popupMenu;
-
 const _ = ExtensionUtils.gettext;
 
 const Voluble = GObject.registerClass(
-class Voluble extends PanelMenu.Button {
-	unmuted = true; // Flag to keep track of Muted/Unmuted state
-	_init() {
-		super._init(0.0, _('Voluble indicator'));
-		this.vol_tmp_filepath = GLib.build_filenamev([GLib.get_user_runtime_dir(), 'voluble.tmp']);
-		this.icon1 = new St.Icon({
-			icon_name: 'audio-volume-overamplified-symbolic-rtl',
-			style_class: 'system-status-icon',
-		});
-		this.add_child(this.icon1);
-		
-		this.itemR = new PopupMenu.PopupMenuItem(_('🐧 Read Selection'));
-		this.itemR.connect('activate', this._readSelection.bind(this));
-		this.menu.addMenuItem(this.itemR);
+    class Voluble extends PanelMenu.Button {
+        unmuted = true;
+        
+        _init() {
+            super._init(0.0, _('Voluble indicator'));
+            this.vol_tmp_filepath = GLib.build_filenamev([GLib.get_user_runtime_dir(), 'voluble.tmp']);
+            
+            this.icon = new St.Icon({
+                icon_name: 'audio-volume-overamplified-symbolic-rtl',
+                style_class: 'system-status-icon',
+            });
+            this.add_child(this.icon);
+            
+            // Read Selection
+            const itemR = new PopupMenu.PopupMenuItem(_('💬 Read Selection'));
+            itemR.connect('activate', this._readSelection.bind(this));
+            this.menu.addMenuItem(itemR);
+            
+            // Summarize Selection
+            this.itemS = new PopupMenu.PopupMenuItem(_('✍ Summarize Selection'));
+            this.itemS.connect('activate', this._summarizeSelection.bind(this));
+            this.menu.addMenuItem(this.itemS);
 
-		this.itemED = new PopupMenu.PopupMenuItem(_('🐧 Mute TTS'));
-		this.itemED.connect('activate', this._toggleTTS.bind(this));
-		this.menu.addMenuItem(this.itemED);
+            // Mute TTS
+            this.itemED = new PopupMenu.PopupMenuItem(_('🔇 Mute TTS'));
+            this.itemED.connect('activate', this._toggleTTS.bind(this));
+            this.menu.addMenuItem(this.itemED);
+            
+            // Shut up
+            this.itemSU = new PopupMenu.PopupMenuItem(_('🛑 Stop Speaking'));
+            this.itemSU.connect('activate', this._shutUP.bind(this));
+            this.menu.addMenuItem(this.itemSU);
 
-		let item2 = new PopupMenu.PopupMenuItem(_('🐧 About'));
-		item2.connect('activate', () => {
-			//Main.notify(_('Voluble announces your desktop notifications out-loud. \n You can find more details and leave feedback on GitHub'));
-			this._notifyVoluble(_('About Voluble.'), _('Voluble announces your desktop notifications out-loud. \n You can find more details and leave feedback on GitHub.'), 'lang-variable-symbolic');
-		});
-		this.menu.addMenuItem(item2);
-	}
+            // About
+            this.itemA = new PopupMenu.PopupMenuItem(_('🦜 About'));
+            this.itemA.connect('activate', () => {
+                this._notifyVoluble(
+                    _('About Voluble.'),
+                    _('Voluble announces your desktop notifications out-loud, can read you the text you selected, or even provide you with a summary, with the help of a local AI. You can find more details and leave feedback on GitHub.'),
+                    'lang-variable-symbolic'
+                );
+            });
+            this.menu.addMenuItem(this.itemA);
+        }
 
-	_toggleTTS() {
-		// Toggle extension state
-		this.unmuted = !this.unmuted;
-		// Update menu item label
-		this.itemED.label.text = _(this.unmuted ? '🐧 Mute TTS' : '🐧 Unmute TTS');
-		this.icon1.set_icon_name(this.unmuted ? 'audio-volume-overamplified-symbolic-rtl' : 'audio-volume-muted-symbolic-rtl'); 
-	}
+        _toggleTTS() {
+            this.unmuted = !this.unmuted;
+            this.itemED.label.text = this.unmuted ? _('🔇 Mute TTS') : _('🔊 Unmute TTS');
+            this.icon.set_icon_name(
+                this.unmuted ? 'audio-volume-overamplified-symbolic-rtl' : 'audio-volume-muted-symbolic-rtl'
+            );
+        }
 
-	_extractNotificationInfo(notification) {
-		// Extract title and description from the notification
-		const title = notification.title;
-		const description =  notification.body || notification.bannerBodyText || ' ';
-		//print(notification.datetime.get_microsecond());
-		try {
-			const file = Gio.File.new_for_path(this.vol_tmp_filepath);
-			const textenc = new TextEncoder();
-			const bytes = textenc.encode(title+' '+description);
-			const [ok, etag] = file.replace_contents(bytes, null, false,
-				Gio.FileCreateFlags.REPLACE_DESTINATION, null);
-			if (ok) {
-				try {
-					const proc = Gio.Subprocess.new(['voluble'],Gio.SubprocessFlags.NONE);
-					const success = proc.wait_check_async(null, null);
-				} catch (e) {
-					logError(e, 'Error spawning voluble!');
-				}
-				}
-		} catch (e) {
-			logError(e, 'I/O error!');
-		}
-	}
+        // Helper function to run voluble with text and arguments
+        _runVoluble(text, args = []) {
+            try {
+                const file = Gio.File.new_for_path(this.vol_tmp_filepath);
+                const encoder = new TextEncoder();
+                const bytes = encoder.encode(text);
+                
+                const [ok, etag] = file.replace_contents(
+                    bytes,
+                    null,
+                    false,
+                    Gio.FileCreateFlags.REPLACE_DESTINATION,
+                    null
+                );
+                
+                if (ok) {
+                    const command = ['voluble'].concat(args);
+                    const proc = Gio.Subprocess.new(command, Gio.SubprocessFlags.NONE);
+                    proc.wait_check_async(null, null);
+                }
+            } catch (e) {
+                logError(e, 'Error running voluble');
+            }
+        }
+        // Function ShutUP, to stop speech currently in progress
+        _shutUP() {
+            const command = ['pkill', '-SIGINT', '(ap|p)lay'];
+            const proc = Gio.Subprocess.new(command, Gio.SubprocessFlags.NONE);
+            proc.wait_check_async(null, null);
+        }
+    
+        _summarizeSelection() {
+            St.Clipboard.get_default().get_text(St.ClipboardType.PRIMARY, (clipboard, text) => {
+                if (text) {
+                    this._runVoluble(text, ['-s']);
+                }
+            });
+        }
 
-	_readSelection() {
-		St.Clipboard.get_default().get_text(St.ClipboardType.PRIMARY, (clipboard, text) => {
-			if (text) {
-				try {
-					const file = Gio.File.new_for_path(this.vol_tmp_filepath);
-					const textenc = new TextEncoder();
-					const bytes = textenc.encode(text);
-					const [ok, etag] = file.replace_contents(bytes, null, false,
-						Gio.FileCreateFlags.REPLACE_DESTINATION, null);
-					if (ok) {
-						try {
-							const proc = Gio.Subprocess.new(['voluble'],Gio.SubprocessFlags.NONE);
-							const success = proc.wait_check_async(null, null);
-						} catch (e) {
-							logError(e, 'Error spawning voluble!');
-						}
-						}
-				} catch (e) {
-					logError(e, 'I/O error!');
-				}
-			}
-		});
-	}
+        _extractNotificationInfo(notification) {
+            const title = notification.title;
+            const description = notification.body || notification.bannerBodyText || ' ';
+            const textToProcess = `${title} ${description}`;
+            
+            this._runVoluble(textToProcess);
+        }
 
-	_notifyVoluble(msg, details, icon) {
-		// Create a custom multiline notification on pressing About
-		let source = new MTray.Source("Voluble Notification", icon);
-		Main.messageTray.add(source);
-		let notification = new MTray.Notification(source, msg, details);
-		notification.setTransient(true);
-		source.showNotification(notification);
-	}
+        _readSelection() {
+            St.Clipboard.get_default().get_text(St.ClipboardType.PRIMARY, (clipboard, text) => {
+                if (text) {
+                    this._runVoluble(text);
+                }
+            });
+        }
 
-});
+        _notifyVoluble(msg, details, icon) {
+            // Create a custom multiline notification on pressing About
+            let source = new MTray.Source("Voluble Notification", icon);
+            Main.messageTray.add(source);
+            let notification = new MTray.Notification(source, msg, details);
+            notification.setTransient(true);
+            source.showNotification(notification);
+        }
+    }
+);
 
 class Extension {
-	constructor(uuid) {
-		this._uuid = uuid;
-		ExtensionUtils.initTranslations(GETTEXT_DOMAIN);
-		this.mtid = null;
-	}
+    constructor(uuid) {
+        this._uuid = uuid;
+        ExtensionUtils.initTranslations(GETTEXT_DOMAIN);
+        this.mtid = null;
+    }
 
 	enable() {
 		this._ctux = new Voluble();
@@ -141,18 +165,18 @@ class Extension {
 		});
 	}
 
-	disable() {
-		if (this._ctux) {
-			this._ctux.destroy();
-			this._ctux = null;
-		}
-		if (this.mtid) { 
-			Main.messageTray.disconnect(this.mtid);
-			this.mtid = null;
-		}
-	}
+    disable() {
+        if (this._ctux) {
+            this._ctux.destroy();
+            this._ctux = null;
+        }
+        if (this.mtid) {
+            Main.messageTray.disconnect(this.mtid);
+            this.mtid = null;
+        }
+    }
 }
 
 function init(meta) {
-	return new Extension(meta.uuid);
+    return new Extension(meta.uuid);
 }
